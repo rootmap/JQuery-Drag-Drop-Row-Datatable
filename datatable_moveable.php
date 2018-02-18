fad75f69ec59fd12984c6a5ff0622c8749a1a5ee
rootmap <f.bhuyian@gmail.com>
2018-02-19 02:07:51 +0600

    Drag & drop full lib with raw source developed modified moveable table

--- datatable_moveable.php
+++ datatable_moveable.php
@@ -75,6 +75,7 @@ tr{
 
 
 </script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
 <script src="datatable-lib/draw-table/datatables/assets/lib/js/jquery.dataTables.min.js?v=v1.2.3"></script>
 <script src="datatable-lib/draw-table/js/jquery.tablednd.js" type="text/javascript"></script>
 
